f25d4e6a81f887e93811885789868258212915b4
younghyun753 <younghyun75398@gmail.com>
2025-05-19 19:57:31 +0900

    feat: JSONProperty 관련 어노테이션 제거

--- src/main/java/com/swmStrong/demo/common/response/ApiResponse.java
+++ src/main/java/com/swmStrong/demo/common/response/ApiResponse.java
@@ -1,17 +1,13 @@
 package com.swmStrong.demo.common.response;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.swmStrong.demo.common.exception.code.ErrorCode;
 import com.swmStrong.demo.common.exception.code.SuccessCode;
 
-@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 
 public record ApiResponse<T>(
-        @JsonProperty("isSuccess")boolean isSuccess,
+        Boolean isSuccess,
         String code,
         String message,
-        @JsonProperty("result")T data
+        T data
 ) {
     public static <T> ApiResponse<T> success(SuccessCode code, T data) {
         return new ApiResponse<>(true, code.getCode(), code.getMessage(), data);

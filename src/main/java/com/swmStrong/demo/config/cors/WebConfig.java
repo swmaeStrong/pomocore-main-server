cff015ba77e7ec6ec3bc0b4c34142c18997c570f
younghyun753 <younghyun75398@gmail.com>
2025-05-22 13:55:09 +0900

    feat: cors 설정 변경

--- src/main/java/com/swmStrong/demo/config/cors/WebConfig.java
+++ src/main/java/com/swmStrong/demo/config/cors/WebConfig.java
@@ -1,5 +1,6 @@
 package com.swmStrong.demo.config.cors;
 
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://3.39.105.127")
+                .allowedOrigins("http://localhost:8080", "http://localhost")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);

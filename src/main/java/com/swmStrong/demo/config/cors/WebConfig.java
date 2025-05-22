5b5b8a25e7eef54d18f4f61835d568a8cfe5f976
younghyun753 <younghyun75398@gmail.com>
2025-05-22 13:35:23 +0900

    feat: 스웨거에서의 요청도 cors 열어주기

--- src/main/java/com/swmStrong/demo/config/cors/WebConfig.java
+++ src/main/java/com/swmStrong/demo/config/cors/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080") // Swagger UI도 허용
+                .allowedOrigins("http://localhost:8080", "http://3.39.105.127")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);

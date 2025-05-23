3da8c23a5f075ca2f4158c4773b89ccb86e00b59
younghyun753 <younghyun75398@gmail.com>
2025-05-23 11:38:39 +0900

    chore: cors 모두 닫아주는 방식으로 변경

--- src/main/java/com/swmStrong/demo/config/cors/WebConfig.java
+++ src/main/java/com/swmStrong/demo/config/cors/WebConfig.java
@@ -1,6 +1,4 @@
 package com.swmStrong.demo.config.cors;
-
-import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +8,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);

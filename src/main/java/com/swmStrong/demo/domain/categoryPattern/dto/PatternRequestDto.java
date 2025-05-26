268fef4ea3539654fa65d71b8fb8bb9e7b13c923
박동현 <jinee9523@gmail.com>
2025-05-26 18:18:44 +0900

    feat: dto 스태틱 메서드 추가

--- src/main/java/com/swmStrong/demo/domain/categoryPattern/dto/PatternRequestDto.java
+++ src/main/java/com/swmStrong/demo/domain/categoryPattern/dto/PatternRequestDto.java
@@ -3,4 +3,7 @@ package com.swmStrong.demo.domain.categoryPattern.dto;
 public record PatternRequestDto(
         String pattern
 ) {
+    public static PatternRequestDto of(String pattern) {
+        return new PatternRequestDto(pattern);
+    }
 }

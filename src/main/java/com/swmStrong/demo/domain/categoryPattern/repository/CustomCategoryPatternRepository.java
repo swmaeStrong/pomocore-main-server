4de4864875edbb90857c899b99f4e2bbac1492cc
박동현 <jinee9523@gmail.com>
2025-05-27 12:47:28 +0900

    refactor: json 중 없는 패턴만 추가

--- src/main/java/com/swmStrong/demo/domain/categoryPattern/repository/CustomCategoryPatternRepository.java
+++ src/main/java/com/swmStrong/demo/domain/categoryPattern/repository/CustomCategoryPatternRepository.java
@@ -1,7 +1,10 @@
 package com.swmStrong.demo.domain.categoryPattern.repository;
 
 
+import java.util.Set;
+
 public interface CustomCategoryPatternRepository {
     void addPattern(String category, String newPattern);
     void removePattern(String category, String pattern);
+    Set<String> findPatternsByCategory(String category);
 }

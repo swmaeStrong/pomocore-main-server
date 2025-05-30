40f796bd90dd872aff93f1675e1a60d1e0df8cc5
박동현 <jinee9523@gmail.com>
2025-05-31 02:01:23 +0900

    remove: 중복 로직 삭제

--- src/main/java/com/swmStrong/demo/config/security/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.swmStrong.demo.config.security.service;
-
-import com.swmStrong.demo.config.security.principal.SecurityPrincipal;
-
-public interface SecurityService {
-    SecurityPrincipal loadUserByUserId(String userId);
-}

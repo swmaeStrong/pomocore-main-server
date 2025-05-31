b070bcb2a1e417729c661c96a91b32c88584e073
younghyun753 <younghyun75398@gmail.com>
2025-06-01 01:19:57 +0900

    feat: 웹훅 로그 저장 및 로그 정보에 따라 구독 연장하는 로직 구현

---
+++ src/main/java/com/swmStrong/demo/domain/web/entity/WebhookLog.java
@@ -0,0 +1,20 @@
+package com.swmStrong.demo.domain.web.entity;
+
+import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Builder
+@Document(collection = "webhook_logs")
+public class WebhookLog {
+    @Id
+    private ObjectId id;
+    private String type;
+    private String timestamp;
+    private String body; // 원본 JSON 문자열로 저장
+    private OffsetDateTime receivedAt;
+}
---
+++ src/main/java/com/swmStrong/demo/web/portone/PortOneWebhookController.java
@@ -0,0 +1,73 @@
+package com.swmStrong.demo.web.portone;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.swmStrong.demo.common.exception.code.SuccessCode;
+import com.swmStrong.demo.common.response.ApiResponse;
+import com.swmStrong.demo.domain.userSubscription.entity.UserSubscription;
+import com.swmStrong.demo.domain.userSubscription.repository.UserSubscriptionRepository;
+import com.swmStrong.demo.domain.userSubscription.service.UserSubscriptionService;
+import com.swmStrong.demo.domain.web.entity.WebhookLog;
+import com.swmStrong.demo.domain.web.repository.WebhookLogRepository;
+import com.swmStrong.demo.infra.portone.PortOneBillingClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+@RestController
+public class PortOneWebhookController {
+    private final UserSubscriptionRepository userSubscriptionRepository;
+    private WebhookLogRepository webhookLogRepository;
+    private UserSubscriptionService userSubscriptionService;
+
+    public PortOneWebhookController(
+            WebhookLogRepository webhookLogRepository,
+            UserSubscriptionService userSubscriptionService, UserSubscriptionRepository userSubscriptionRepository) {
+        this.webhookLogRepository = webhookLogRepository;
+        this.userSubscriptionService = userSubscriptionService;
+        this.userSubscriptionRepository = userSubscriptionRepository;
+    }
+
+    @PostMapping("/webhook/portone")
+    public void handlePortOneWebhook(@RequestBody Map<String, Object> payload)
+    {
+        // 올바르게 갱신된 경우 -> 유저의 기존 구독 정보 만료로 시켜주고, 바꿔주고 다시 결제 예약
+        try {
+            // 우선 몽고 디비로 로그 다 넘기기
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode root = objectMapper.valueToTree(payload);
+            String type = root.has("type") ? root.get("type").asText() : "";
+            String timestamp = root.has("timestamp") ? root.get("timestamp").asText() : "";
+
+            WebhookLog log = WebhookLog.builder()
+                    .type(type)
+                    .timestamp(timestamp)
+                    .body(root.asText()) // 원본 그대로 저장
+                    .receivedAt(OffsetDateTime.now())
+                    .build();
+
+            webhookLogRepository.save(log);
+
+
+
+            JsonNode dataNode = root.get("data");
+            String paymentId = dataNode.get("paymentId").asText();
+            String userId = "나중에 토큰으로 까서 가져오는 걸로 교체";
+            switch (type){
+                case "Transaction.Paid":
+                    // 구독제 요금일 경우에는 연장 로직 수행
+                    if (userSubscriptionRepository.existsByPaymentId(paymentId)) {
+                        userSubscriptionService.extendUserSubscription(userId, paymentId);
+                    }
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+}

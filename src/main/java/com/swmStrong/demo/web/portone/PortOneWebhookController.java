a35a1294478ebbbdeb3cacbebfd105fa6c10e641
younghyun753 <younghyun75398@gmail.com>
2025-06-01 02:35:46 +0900

    chore: 문서화 보충

--- src/main/java/com/swmStrong/demo/web/portone/PortOneWebhookController.java
+++ src/main/java/com/swmStrong/demo/web/portone/PortOneWebhookController.java
@@ -1,15 +1,11 @@
 package com.swmStrong.demo.web.portone;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.swmStrong.demo.common.exception.code.SuccessCode;
-import com.swmStrong.demo.common.response.ApiResponse;
-import com.swmStrong.demo.domain.userSubscription.entity.UserSubscription;
 import com.swmStrong.demo.domain.userSubscription.repository.UserSubscriptionRepository;
 import com.swmStrong.demo.domain.userSubscription.service.UserSubscriptionService;
 import com.swmStrong.demo.domain.web.entity.WebhookLog;
 import com.swmStrong.demo.domain.web.repository.WebhookLogRepository;
-import com.swmStrong.demo.infra.portone.PortOneBillingClient;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.OffsetDateTime;
 import java.util.Map;
 
+@Tag(name = "포트원 웹훅 컨트롤러 ( 서버 결제 예약 및 로그 남기는 용도 )")
 @RestController
 public class PortOneWebhookController {
     private final UserSubscriptionRepository userSubscriptionRepository;
-    private WebhookLogRepository webhookLogRepository;
-    private UserSubscriptionService userSubscriptionService;
+    private final WebhookLogRepository webhookLogRepository;
+    private final UserSubscriptionService userSubscriptionService;
 
     public PortOneWebhookController(
             WebhookLogRepository webhookLogRepository,
-            UserSubscriptionService userSubscriptionService, UserSubscriptionRepository userSubscriptionRepository) {
+            UserSubscriptionService userSubscriptionService,
+            UserSubscriptionRepository userSubscriptionRepository) {
         this.webhookLogRepository = webhookLogRepository;
         this.userSubscriptionService = userSubscriptionService;
         this.userSubscriptionRepository = userSubscriptionRepository;
@@ -58,9 +56,9 @@ public class PortOneWebhookController {
             String userId = "나중에 토큰으로 까서 가져오는 걸로 교체";
             switch (type){
                 case "Transaction.Paid":
-                    // 구독제 요금일 경우에는 연장 로직 수행
+                    // 구독제 요금 결제 완료일 경우에는 이후 다음 구독 예약 결제 로직 수행
                     if (userSubscriptionRepository.existsByPaymentId(paymentId)) {
-                        userSubscriptionService.extendUserSubscription(userId, paymentId);
+                        userSubscriptionService.scheduleUserSubscription(userId, paymentId);
                     }
             }
 

a5992e8ff002c83de7a667d0c4964f977ce8a47d
박동현 <jinee9523@gmail.com>
2025-06-03 02:03:05 +0900

    chore: 미사용 의존성 삭제

--- src/main/java/com/swmStrong/demo/domain/user/facade/UserInfoProvider.java
+++ src/main/java/com/swmStrong/demo/domain/user/facade/UserInfoProvider.java
@@ -1,7 +1,5 @@
 package com.swmStrong.demo.domain.user.facade;
 
-import com.swmStrong.demo.domain.user.entity.User;
-
 public interface UserInfoProvider {
     String getNicknameByUserId(String userId);
 }

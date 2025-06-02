af1fd6501a5c8ed4c294e9bee411812e64df185f
younghyun753 <younghyun75398@gmail.com>
2025-06-02 23:56:20 +0900

    chore: 유저 구독 중복 방지 로직 적용 & 유저가 하나의 구독 상태만을 갖도록 수정

--- src/main/java/com/swmStrong/demo/domain/userPaymentMethod/repository/UserPaymentMethodRepository.java
+++ src/main/java/com/swmStrong/demo/domain/userPaymentMethod/repository/UserPaymentMethodRepository.java
@@ -4,8 +4,11 @@ import com.swmStrong.demo.domain.userPaymentMethod.entity.UserPaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserPaymentMethodRepository extends JpaRepository<UserPaymentMethod, String> {
     UserPaymentMethod findByUser(User user);
     boolean existsByBillingKeyAndUserId(String billingKey, String userId);
+    Optional<UserPaymentMethod> findByBillingKeyAndUserId(String billingKey, String userId);
 }
--- src/main/java/com/swmStrong/demo/domain/userSubscription/repository/UserSubscriptionRepository.java
+++ src/main/java/com/swmStrong/demo/domain/userSubscription/repository/UserSubscriptionRepository.java
@@ -1,11 +1,20 @@
 package com.swmStrong.demo.domain.userSubscription.repository;
 
+import com.swmStrong.demo.domain.user.entity.User;
 import com.swmStrong.demo.domain.userSubscription.entity.UserSubscription;
+import com.swmStrong.demo.domain.userSubscription.entity.UserSubscriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface UserSubscriptionRepository extends JpaRepository<UserSubscription, String> {
     UserSubscription findByPaymentId(String paymentId);
-    boolean existsByPaymentId(String paymentId);
+    UserSubscription findByUserId(String userId);
+    List<UserSubscription> findByUserSubscriptionStatusAndEndTimeBefore(UserSubscriptionStatus userSubscriptionStatus, LocalDateTime now);
+    boolean existsByUserSubscriptionStatusAndUserId(UserSubscriptionStatus userSubscriptionStatus, String userId);
+    List<UserSubscription> findByUserSubscriptionStatusInAndUserId(
+            List<UserSubscriptionStatus> userSubscriptionStatusList, String userId);
 }

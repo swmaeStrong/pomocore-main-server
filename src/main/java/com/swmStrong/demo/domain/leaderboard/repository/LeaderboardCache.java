06f7612871fc7f587d8ce7952e0ca583da49c0c2
박동현 <jinee9523@gmail.com>
2025-06-04 18:18:53 +0900

    move: Leaderboard entity 추가로 인한 cache-repository 분리

--- src/main/java/com/swmStrong/demo/domain/leaderboard/repository/LeaderboardCache.java
+++ src/main/java/com/swmStrong/demo/domain/leaderboard/repository/LeaderboardCache.java
@@ -5,7 +5,7 @@ import org.springframework.data.redis.core.ZSetOperations;
 
 import java.util.Set;
 
-public interface LeaderboardRepository {
+public interface LeaderboardCache {
     void increaseScoreByUserId(String key, String userId, double seconds);
     Set<ZSetOperations.TypedTuple<String>> findPageWithSize(String key, int page, int size);
     Long findRankByUserId(String key, String userId);

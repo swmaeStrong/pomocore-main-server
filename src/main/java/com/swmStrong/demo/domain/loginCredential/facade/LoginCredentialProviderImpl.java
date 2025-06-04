a0594e90082b089d70ed2e381f4673e7db6da54c
박동현 <jinee9523@gmail.com>
2025-06-04 15:56:46 +0900

    refactor: 로그인 시 2번 발생하던 쿼리 1번으로 개선

--- src/main/java/com/swmStrong/demo/domain/loginCredential/facade/LoginCredentialProviderImpl.java
+++ src/main/java/com/swmStrong/demo/domain/loginCredential/facade/LoginCredentialProviderImpl.java
@@ -4,6 +4,8 @@ import com.swmStrong.demo.config.security.principal.SecurityPrincipal;
 import com.swmStrong.demo.domain.global.Role;
 import com.swmStrong.demo.domain.loginCredential.entity.LoginCredential;
 import com.swmStrong.demo.domain.loginCredential.repository.LoginCredentialRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,29 +23,28 @@ public class LoginCredentialProviderImpl implements LoginCredentialProvider {
     }
 
     @Override
-    public SecurityPrincipal loadPrincipalByEmail(String email) {
+    public Role loadRoleByUserId(String userId) {
+        LoginCredential loginCredential = loginCredentialRepository.findById(userId)
+                .orElseThrow(IllegalArgumentException::new);
+        return loginCredential.getRole();
+    }
+
+    @Override
+    public SecurityPrincipal loadPrincipalByToken(UsernamePasswordAuthenticationToken token) {
+        String email = token.getName();
+        String password = token.getCredentials().toString();
+
         LoginCredential loginCredential = loginCredentialRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("등록되지 않은 이메일 입니다."));
+
+        if (!passwordEncoder.matches(password, loginCredential.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 틀렸습니다.");
+        }
 
         return SecurityPrincipal.builder()
                 .userId(loginCredential.getId())
                 .nickname(loginCredential.getNickname())
                 .grantedAuthority(new SimpleGrantedAuthority(loginCredential.getRole().getAuthority()))
                 .build();
-    }
-
-    @Override
-    public boolean isPasswordMatched(String email, String password) {
-        LoginCredential loginCredential = loginCredentialRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("등록되지 않은 이메일입니다."));
-
-        return passwordEncoder.matches(password, loginCredential.getPassword());
-    }
-
-    @Override
-    public Role loadRoleByUserId(String userId) {
-        LoginCredential loginCredential = loginCredentialRepository.findById(userId)
-                .orElseThrow(IllegalArgumentException::new);
-        return loginCredential.getRole();
     }
 }

941785f73488bd4b878ff68896a489c3efa1fb88
박동현 <jinee9523@gmail.com>
2025-06-06 00:56:10 +0900

    feat: 소셜 로그인 관련 칼럼 추가

--- src/main/java/com/swmStrong/demo/domain/loginCredential/entity/LoginCredential.java
+++ src/main/java/com/swmStrong/demo/domain/loginCredential/entity/LoginCredential.java
@@ -1,12 +1,17 @@
 package com.swmStrong.demo.domain.loginCredential.entity;
 
+import com.swmStrong.demo.domain.common.enums.Role;
+import com.swmStrong.demo.domain.user.dto.UserRequestDto;
 import com.swmStrong.demo.domain.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -18,6 +23,12 @@ public class LoginCredential extends User {
 
     private String password;
 
+    // 소셜로 시작한 경우 true로, 이 때 일반 로그인이 아예 되지 않도록 막기
+    @Column(name = "is_social", nullable = false, columnDefinition = "BOOLEAN DEFAULT 0")
+    private boolean isSocial;
+
+    private String socialId;
+
     private void setUser(User user) {
         this.id = user.getId();
         this.nickname = user.getNickname();
@@ -28,5 +39,31 @@ public class LoginCredential extends User {
         setUser(user);
         this.email = email;
         this.password = password;
+        this.isSocial = false;
+        this.updateRole(Role.USER);
+    }
+
+    private void setSocial() {
+        this.isSocial = true;
+    }
+
+    private void setSocialId(String socialId) {
+        this.socialId = socialId;
+    }
+
+    public LoginCredential connectSocialAccount(String socialId) {
+        this.setSocialId(socialId);
+        this.updateRole(Role.USER);
+        return this;
+    }
+
+    public static LoginCredential createSocialLoginCredential(String email, String socialId) {
+        LoginCredential loginCredential = LoginCredential.builder()
+                .user(User.of(UserRequestDto.of(UUID.randomUUID().toString(), "")))
+                .email(email)
+                .build();
+        loginCredential.setSocial();
+        loginCredential.setSocialId(socialId);
+        return loginCredential;
     }
 }

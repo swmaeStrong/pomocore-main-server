581bf0f5667c9d732328f8dbb468247e07fd4fd6
박동현 <jinee9523@gmail.com>
2025-06-05 21:47:25 +0900

    chore: whiteList 수정

--- src/main/java/com/swmStrong/demo/config/security/WhiteListConfig.java
+++ src/main/java/com/swmStrong/demo/config/security/WhiteListConfig.java
@@ -18,6 +18,7 @@ public class WhiteListConfig {
 
     public static final String[] WHITE_LIST_FOR_GET = {
             PREFIX+"/category",
-            PREFIX+"/category/**"
+            PREFIX+"/category/**",
+            PREFIX+"/usage-log",
     };
 }

21222fdbcdf3d2913337e62daf5510bb31d1d96c
박동현 <jinee9523@gmail.com>
2025-06-16 23:40:08 +0900

    feat: UUID 포맷 validation

--- src/main/java/com/swmStrong/demo/domain/user/dto/UserRequestDto.java
+++ src/main/java/com/swmStrong/demo/domain/user/dto/UserRequestDto.java
@@ -1,6 +1,12 @@
 package com.swmStrong.demo.domain.user.dto;
 
+import jakarta.validation.constraints.Pattern;
+
 public record UserRequestDto(
+        @Pattern(
+                regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$",
+                message = "올바른 UUID 포맷이 아닙니다."
+        )
         String userId
 ) {
     public static UserRequestDto of(String userId) {

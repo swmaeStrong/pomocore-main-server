d3243d3407c55e16c5ee48695ea63c7cd8f1234e
박동현 <jinee9523@gmail.com>
2025-06-24 01:32:11 +0900

    feat: timezone 설정

--- src/main/java/com/swmStrong/demo/DemoApplication.java
+++ src/main/java/com/swmStrong/demo/DemoApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 import com.swmStrong.demo.infra.LLM.LLMConfig;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties(LLMConfig.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

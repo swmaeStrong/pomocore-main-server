3363424d305eb599698e16277286b977a30368b9
박동현 <jinee9523@gmail.com>
2025-07-10 20:59:03 +0900

    fix: redis sortedSet 중복 저장 에러

--- src/main/java/com/swmStrong/demo/message/dto/PatternClassifyMessage.java
+++ src/main/java/com/swmStrong/demo/message/dto/PatternClassifyMessage.java
@@ -7,14 +7,16 @@ public record PatternClassifyMessage(
         String app,
         String title,
         String url,
-        String categoryId
+        String categoryId,
+        Double margin
 ) {
     @Builder
-    public PatternClassifyMessage(String usageLogId, String app, String title, String url, String categoryId) {
+    public PatternClassifyMessage(String usageLogId, String app, String title, String url, String categoryId, Double margin) {
         this.usageLogId = usageLogId;
         this.app = app;
         this.title = title;
         this.url = url;
         this.categoryId = categoryId;
+        this.margin = margin;
     }
 }

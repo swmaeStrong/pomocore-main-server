cb5b8882217052dde4d76cb47c2a6acafd6c8418
박동현 <jinee9523@gmail.com>
2025-07-30 16:28:03 +0900

    feat: sessionScore 접근 시 db 락

--- src/main/java/com/swmStrong/demo/domain/sessionScore/repository/SessionScoreRepository.java
+++ src/main/java/com/swmStrong/demo/domain/sessionScore/repository/SessionScoreRepository.java
@@ -16,4 +16,7 @@ public interface SessionScoreRepository extends JpaRepository<SessionScore, Long
     
     @Query("SELECT MAX(s.session) FROM SessionScore s WHERE s.user.id = :userId AND s.sessionDate = :sessionDate")
     Optional<Integer> findMaxSessionByUserIdAndSessionDate(@Param("userId") String userId, @Param("sessionDate") LocalDate sessionDate);
+    
+    @Query(value = "SELECT s.session FROM session_score s WHERE s.user_id = :userId AND s.session_date = :sessionDate ORDER BY s.session DESC LIMIT 1 FOR UPDATE", nativeQuery = true)
+    Optional<Integer> findMaxSessionByUserIdAndSessionDateWithLock(@Param("userId") String userId, @Param("sessionDate") LocalDate sessionDate);
 }

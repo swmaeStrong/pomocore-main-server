179066547ade605ed2abf92780c0ea21f0ed0efa
박동현 <jinee9523@gmail.com>
2025-08-08 17:44:47 +0900

    remove: 미사용 도메인 정리

--- src/main/java/com/swmStrong/demo/domain/usageLog/dto/CategoryUsageDto.java
+++
@@ -1,7 +0,0 @@
-package com.swmStrong.demo.domain.usageLog.dto;
-
-public record CategoryUsageDto(
-        String category,
-        double duration
-) {
-}
--- src/main/java/com/swmStrong/demo/domain/usageLog/facade/UsageLogUpdateProvider.java
+++
@@ -1,9 +0,0 @@
-package com.swmStrong.demo.domain.usageLog.facade;
-
-import com.swmStrong.demo.domain.usageLog.entity.UsageLog;
-import org.bson.types.ObjectId;
-
-public interface UsageLogUpdateProvider {
-    UsageLog updateCategory(ObjectId usageLogId, ObjectId categoryPatternId);
-    UsageLog loadByUsageLogId(ObjectId usageLogId);
-}

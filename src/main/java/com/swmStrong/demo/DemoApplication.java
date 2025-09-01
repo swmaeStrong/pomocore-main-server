c9e2ce2eb25b1403e00a81ff41e37eaa731b6304
박동현 <jinee9523@gmail.com>
2025-09-01 18:46:53 +0900

    feat: key 단수형으로 변경

--- src/main/java/com/swmStrong/demo/DemoApplication.java
+++ src/main/java/com/swmStrong/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import com.swmStrong.demo.infra.LLM.GeminiConfig;
 import com.swmStrong.demo.infra.LLM.OpenAIConfig;
 
 import java.util.TimeZone;
@@ -13,7 +12,7 @@ import java.util.TimeZone;
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication(exclude = {io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration.class})
-@EnableConfigurationProperties({GeminiConfig.class, OpenAIConfig.class})
+@EnableConfigurationProperties({OpenAIConfig.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
